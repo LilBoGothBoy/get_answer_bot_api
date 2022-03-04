46dba3d4d9a3d030eb22030d960bcf88c82b255f
LilBoGothBoy <74736733+LilBoGothBoy@users.noreply.github.com>
2022-03-04 14:12:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
-import pandas as pd
-import logging
+from re import compile, sub, escape
+from string import punctuation
+from flask_restful import Api, Resource
+from flask import Flask
+from pymorphy2 import MorphAnalyzer
 import nltk
-import sys
 nltk.download("stopwords")
 from nltk.corpus import stopwords
-from pymorphy2 import MorphAnalyzer
-from flask import Flask
-from flask_restful import Api, Resource
-from string import punctuation
-from re import compile, sub, escape
+import pandas as pd
+import logging
+import sys
 
 
 
@@ -20,14 +20,14 @@ app.logger.setLevel(logging.ERROR)
 
 class Bot_Answer(Resource):
     def get(self, user_answer):
-        data = pd.read_excel("data.xlsx")
-        stop_words = stopwords.words("russian")
-        user_answer = compile("<.*?>").sub("", user_answer)
-        user_answer = compile("[%s]" % escape(
-            punctuation)).sub(" ", user_answer)
-        user_answer = sub("\s+", " ", user_answer)
-        user_answer = sub(r"[^\w\s]", "", str(user_answer))
-        user_answer = sub(r"\s+", " ", user_answer)
+        data = pd.read_excel('data.xlsx')
+        stop_words = stopwords.words('russian')
+        user_answer = compile('<.*?>').sub('', user_answer)
+        user_answer = compile('[%s]' % escape(
+            punctuation)).sub(' ', user_answer)
+        user_answer = sub('\s+', ' ', user_answer)
+        user_answer = sub(r'[^\w\s]', '', str(user_answer))
+        user_answer = sub(r'\s+', ' ', user_answer)
         morph = MorphAnalyzer()
         stem_answer_list = [morph.parse(
             word)[0].normal_form for word in user_answer.split()]
@@ -36,30 +36,30 @@ class Bot_Answer(Resource):
             if word in stop_words:
                 del stem_answer_list[stem_answer_list.index(word)]
         for word in stem_answer_list:
-            if "1" in stem_answer_list and word in data[1][2]:
-                return "РЖД Техническая поддержка", 200
+            if '1' in stem_answer_list and word in data[1][2]:
+                return 'RZH', 200
 
         for word in stem_answer_list:
-            if "1" in stem_answer_list and word in data[1][1]:
-                return "РЖД Акции и скидки", 200
+            if '1' in stem_answer_list and word in data[1][1]:
+                return 'RZH', 200
 
         for word in stem_answer_list:
-            if "1" in stem_answer_list and word in data[1][0]:
-                return "РЖД Билеты и расписание", 200
+            if '1' in stem_answer_list and word in data[1][0]:
+                return 'RZH', 200
 
         for word in stem_answer_list:
-            if "2" in stem_answer_list and word in data[2][1]:
-                return "СБЕР Условия доставки", 200
+            if '2' in stem_answer_list and word in data[2][1]:
+                return 'RZH', 200
         for word in stem_answer_list:
-            if "2" in stem_answer_list and word in data[2][2]:
-                return "СБЕР Способы оплаты", 200
+            if '2' in stem_answer_list and word in data[2][2]:
+                return 'RZH', 200
 
         for word in stem_answer_list:
-            if "2" in stem_answer_list and word in data[2][0]:
-                return "СБЕР Оформление заказа", 200
-        return "Совпадение не найдено", 404
+            if '2' in stem_answer_list and word in data[2][0]:
+                return 'RZH', 200
+        return 'RZH', 404
 
 
-api.add_resource(Bot_Answer, "/get_answer/<string:user_answer>")
-if __name__ == "__main__":
+api.add_resource(Bot_Answer, '/get_answer/<string:user_answer>')
+if __name__ == '__main__':
     app.run(debug=True)
